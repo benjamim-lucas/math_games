d2401086cb63a4115356fa4ce4a896c1454a40c9
Ben <benjamimlucasrodrigues@gmail.com>
2025-07-29 16:18:14 -0300

    Enhance guessing and multiplying games to display the number of attempts

--- guessing.py
+++ guessing.py
@@ -2,11 +2,14 @@ import random
 
 guess = 0
 correct_number = random.randint(1, 10)
+attempts = 0
+
 while guess != correct_number:
+    attempts += 1
     guess = int(input("Guess a number between 1 and 10: "))
     if guess < correct_number:
         print("Too low! Try again.")
     elif guess > correct_number:
         print("Too high! Try again.")
     else:
-        print("Congratulations! You've guessed the correct number.")
+        print(f"Congratulations! You got it with {attempts} attempts!")
